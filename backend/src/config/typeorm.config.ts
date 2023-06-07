2207da27614147fdd1ed6c09d2ae3647ad560f3e
Coraline <cesedji@myges.fr>
2023-06-07 22:35:37 +0200

    Feat : update db

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,22 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Answer } from 'src/domains/answers/answers.entity';
 import { Article } from 'src/domains/articles/article.entity';
 import { Badge } from 'src/domains/badges/badges.entity';
+import { BoxeIdeas } from 'src/domains/boxe_ideas/boxeIdeas.entity';
 import { Category } from 'src/domains/categories/categories.entity';
 import { Choice } from 'src/domains/choices/choices.entity';
-import { Client } from 'src/domains/clients/clients.entity';
 import { Comment } from 'src/domains/comment/comment.entity';
 import { Mission } from 'src/domains/missions/missions.entity';
+import { Question } from 'src/domains/questions/question.entity';
 import { Quest } from 'src/domains/quests/quests.entity';
+import { Requirement } from 'src/domains/requirements/requirements.entity';
 import { Review } from 'src/domains/reviews/reviews.entity';
 import { Skill } from 'src/domains/skills/skills.entity';
+import { Suggestion } from 'src/domains/suggestions/suggestion.entity';
 import { Survey } from 'src/domains/surveys/survey.entity';
 import { UserQuest } from 'src/domains/user_quests/userQuest.entity';
 import { User } from 'src/domains/users/users.entity';
 import { Vote } from 'src/domains/votes/votes.entity';
-import { Requirement } from 'src/domains/requirements/requirements.entity';
-import { Question } from 'src/domains/questions/question.entity';
-import { Answer } from 'src/domains/answers/answers.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -30,7 +31,6 @@ import { Answer } from 'src/domains/answers/answers.entity';
         Category,
         User,
         UserQuest,
-        Client,
         Mission,
         Choice,
         Review,
@@ -44,6 +44,8 @@ import { Answer } from 'src/domains/answers/answers.entity';
         Requirement,
         Question,
         Answer,
+        Suggestion,
+        BoxeIdeas,
       ],
       synchronize: process.env.ENV !== 'production',
     }),
--- backend/src/domains/users/users.service.ts
+++ backend/src/domains/users/users.service.ts
@@ -1,18 +1,48 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdateUserDto } from 'src/domains/users/users.dto';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
-import { User } from './users.entity';
+import { RoleUser, User } from './users.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @Inject(REQUEST) private request: Request,
   ) {}
 
   findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+    const user = this.request['user'] as User;
+
+    if (user.role === RoleUser.ADMIN)
+      return this.usersRepository.find({
+        where: { deletedAt: null },
+      });
+
+    if (user.role === RoleUser.CLIENT) {
+      //TODO
+      /*  const missions = await this.missionsRepository.find({
+        where: { clientId: user.id, deletedAt: null },
+      });
+      return missions.map((mission) => mission.consultant); */
+    }
+
+    if (user.role === RoleUser.CONSULTANT) {
+      //TODO
+      /*   const missions = await this.missionsRepository.find({
+        where: { consultantId: user.id, deletedAt: null },
+      });
+      return missions.map((mission) => mission.client); */
+    }
+
+    /*  if (user.role === RoleUser.CONSULTANT)
+      return .createQueryBuilder('user')
+      .leftJoinAndSelect('user.clientMissions', 'mission')
+      .where('mission.clientId = :clientId', { clientId: user.id })
+      .andWhere('mission.deletedAt IS NULL')
+      .getMany(); */
   }
 
   async find(id: string): Promise<User> {
