cda3061ac1945657568847d443c9daa2eaea281b
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-06-08 14:04:22 +0200

    fix: user-quest dto

--- backend/src/domains/user_quests/userQuest.dto.ts
+++ backend/src/domains/user_quests/userQuest.dto.ts
@@ -1,9 +1,6 @@
 import { IsEnum, IsOptional, IsUUID, ValidateNested } from 'class-validator';
 import { UserQuestStatus } from '../user_quests/userQuest.entity';
 import { Badge } from '../badges/badges.entity';
-import { User } from '../users/users.entity';
-import { Quest } from '../quests/quests.entity';
-import { Review } from '../reviews/reviews.entity';
 
 export class CreateUserQuestDto {
   @IsEnum(UserQuestStatus)
@@ -12,18 +9,6 @@ export class CreateUserQuestDto {
   @IsOptional()
   @ValidateNested()
   badge?: Badge;
-
-  @IsOptional()
-  @ValidateNested({ each: true })
-  users?: User[];
-
-  @IsOptional()
-  @ValidateNested({ each: true })
-  quests?: Quest[];
-
-  @IsOptional()
-  @ValidateNested({ each: true })
-  reviews?: Review[];
 }
 
 export class UpdateUserQuestDto {
@@ -37,16 +22,4 @@ export class UpdateUserQuestDto {
   @IsOptional()
   @ValidateNested()
   badge?: Badge;
-
-  @IsOptional()
-  @ValidateNested({ each: true })
-  users?: User[];
-
-  @IsOptional()
-  @ValidateNested({ each: true })
-  quests?: Quest[];
-
-  @IsOptional()
-  @ValidateNested({ each: true })
-  reviews?: Review[];
 }
