fd5ee38c467621adfd1bfed3f8a17477c731015b
Océane <oceane.renoux@hotmail.fr>
2023-06-08 23:09:02 +0200

    feat : add articles by categories

--- backend/src/domains/categories/categories.service.ts
+++ backend/src/domains/categories/categories.service.ts
@@ -28,6 +28,9 @@ export class CategoriesService {
         id,
         deletedAt: null,
       },
+      relations: {
+        articles: true,
+      },
     });
 
     if (!data) throw new NotFoundException();
